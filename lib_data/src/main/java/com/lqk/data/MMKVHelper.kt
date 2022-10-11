c8c0fa9f4318096860fa50ce7a4b990dafc3f513
李乾坤 <liqiankun1229@gmail.com>
2022-10-11 23:16:04 +0800

    2022/10/11

--- lib_data/src/main/java/com/lqk/data/MMKVHelper.kt
+++ lib_data/src/main/java/com/lqk/data/MMKVHelper.kt
@@ -1,6 +1,8 @@
 package com.lqk.data
 
 import android.app.Application
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.Log
 import com.tencent.mmkv.MMKV
 
@@ -37,6 +39,70 @@ object MMKVHelper {
      */
     fun gainString(k: String, defaultStr: String = ""): String {
         return mmkv?.decodeString(k) ?: defaultStr
+    }
+
+    fun saveInt(k: String, v: Int) {
+        mmkv?.encode(k, v)
+    }
+
+    fun gainInt(k: String, defaultInt: Int): Int {
+        return mmkv?.decodeInt(k, defaultInt) ?: defaultInt
+    }
+
+    fun saveLong(k: String, v: Long) {
+        mmkv?.encode(k, v)
+    }
+
+    fun gainLong(k: String, defaultLong: Long): Long {
+        return mmkv?.decodeLong(k, defaultLong) ?: defaultLong
+    }
+
+    fun saveFloat(k: String, v: Float) {
+        mmkv?.encode(k, v)
+    }
+
+    fun gainFloat(k: String, defaultFloat: Float): Float {
+        return mmkv?.decodeFloat(k, defaultFloat) ?: defaultFloat
+    }
+
+    fun saveDouble(k: String, v: Double) {
+        mmkv?.encode(k, v)
+    }
+
+    fun gainDouble(k: String, defaultDouble: Double): Double {
+        return mmkv?.decodeDouble(k, defaultDouble) ?: defaultDouble
+    }
+
+    fun saveBoolean(k: String, v: Boolean) {
+        mmkv?.encode(k, v)
+    }
+
+    fun gainBoolean(k: String, defaultBoolean: Boolean): Boolean {
+        return mmkv?.getBoolean(k, defaultBoolean) ?: defaultBoolean
+    }
+
+    fun saveByteArray(k: String, v: ByteArray) {
+        mmkv?.encode(k, v)
+    }
+
+    fun gainByteArray(k: String, defaultByteArray: ByteArray): ByteArray {
+        return mmkv?.decodeBytes(k, defaultByteArray) ?: defaultByteArray
+    }
+
+    fun <T : Parcelable> saveParcelable(k: String, v: T) {
+        mmkv?.encode(k, v)
+    }
+
+    fun <T : Parcelable> gainParcelable(k: String, defaultParcelable: Class<T>): T? {
+        return mmkv?.decodeParcelable(k, defaultParcelable)
+    }
+
+    fun saveSetString(k: String, v: Set<String>) {
+        mmkv?.encode(k, v)
+    }
+
+    fun gainSetString(k: String, defaultSetString: Set<String>): Set<String> {
+        return mmkv?.decodeStringSet(k, defaultSetString) ?: defaultSetString
     }
 
 }
--- lib_data/src/main/java/com/lqk/data/RoomHelper.kt
+++ lib_data/src/main/java/com/lqk/data/RoomHelper.kt
@@ -3,7 +3,7 @@ package com.lqk.data
 /**
  * @author LQK
  * @time 2022/9/30 21:58
- *
+ * 数据库
  */
 class RoomHelper {
 }
